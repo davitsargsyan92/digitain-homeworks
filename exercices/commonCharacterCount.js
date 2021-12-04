5a8cbfee8f4149debcafc462eab7f17733f1c9ec
haykbaghdasaryan1@gmail.com <haykbaghdasaryan1@gmail.com>
2021-12-04 15:16:27 +0400

    homework 2

--- exercices/commonCharacterCount.js
+++ exercices/commonCharacterCount.js
@@ -1,8 +1,5 @@
-// Given two strings, find the number of common characters between them.
-
-// For s1 = "aabcc" and s2 = "adcaa", the output should be
-// solution(s1, s2) = 3.
+che 
 
 function getCommonCharactersCount(s1, s2) {
-    
+    console.log(1*3)
 }
