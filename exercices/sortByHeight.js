90b27fce9d2a17d7a6a7ae86162850da5d617b0d
Hovo <hovhannesdadayan@gmail.com>
2021-12-05 07:07:31 +0400

    homework 1

--- exercices/sortByHeight.js
+++ exercices/sortByHeight.js
@@ -1,16 +1,12 @@
-    // Some people are standing in a row in a park. There are trees between them which cannot be moved.
-    // Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
-    // People can be very tall!
+// Some people are standing in a row in a park. There are trees between them which cannot be moved.
+// Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
+// People can be very tall!
 
+//EXAMPLE
 
-    //EXAMPLE
+// For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
+// sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190]
 
-    // For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
-    // sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190]
-
-
-    function sortByHeight(a) {
-            
-    }
-
-    
+function sortByHeight(a) {
+  console.log("homework");
+}
