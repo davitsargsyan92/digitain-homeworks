88fc8ace85595d54dfc2ff6c19a32567a71fe4cf
Artur <sargsyanartur117@gmail.com>
2021-12-10 23:27:36 +0400

    Exercices 1, commonCharacterCount

--- exercices/1.commonCharacterCount.js
+++ exercices/1.commonCharacterCount.js
@@ -4,5 +4,25 @@
 // solution(s1, s2) = 3.
 
 function getCommonCharactersCount(s1, s2) {
-    console.log("getCommonCharactersCount")
+    var ob1 = {};
+    var count = 0;
+
+    for (let i = 0; i < s1.length; i++) {
+        if(s1[i] in ob1){
+            ob1[s1[i]]++;
+        }else{
+            ob1[s1[i]] = 1;
+        }
+    }
+    for (let i = 0; i < s2.length; i++) {
+        if(s2[i] in ob1){
+            count++;
+            ob1[s2[i]]--;
+            if(ob1[s2[i]] === 0){
+                delete ob1[s2[i]];
+            }
+        }
+    }
+    console.log(count)
 }
+
