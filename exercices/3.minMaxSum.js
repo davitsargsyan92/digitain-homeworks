d4fcdb02a9480d92aa5bbd2550b56856282455cc
Hovo <hovhannesdadayan@gmail.com>
2021-12-13 02:40:02 +0400

    Min and max sum

--- exercices/3.minMaxSum.js
+++ exercices/3.minMaxSum.js
@@ -6,7 +6,15 @@
 
 // The minimum sum is 1+ 3 + 5 + 7  and the maximum sum is 3+5+7+9. The function prints 16 24
 
-
-function minMaxSum(arr){
-
+function minMaxSum(arr) {
+  let sum = 0;
+  let min = 0;
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    min = sum - Math.max(...arr);
+    max = sum - Math.min(...arr);
+  }
+  console.log(min, max);
 }
+console.log(minMaxSum([1, 3, 5, 7, 9]));
