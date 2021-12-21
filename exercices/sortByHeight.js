f66e8dde86793ebf468b892a486b303ab6f60c9a
Anya Kosyan <anya.kosyan@digitain.com>
2021-12-22 01:07:04 +0400

    sort by height

--- exercices/sortByHeight.js
+++ exercices/sortByHeight.js
@@ -8,9 +8,20 @@
     // For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
     // sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190]
 
-
     function sortByHeight(a) {
-            
+        const copyArr = a
+        const sortedArr = copyArr.sort() 
+        const sortedByHeightArr = []
+        for(let i = a.length-1; i >= 0; i--) {
+            if(a[i] !== -1) {
+                sortedByHeightArr[i] = sortedArr[sortedArr.length - 1]
+                sortedArr.pop()
+            } else {
+                sortedByHeightArr[i] = -1
+            }
+        }
+
+        return sortedByHeightArr
     }
 
     
