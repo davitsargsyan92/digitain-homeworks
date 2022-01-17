4c7313b4c9745c27b025c9e59211357172dd8bac
taguhi.hakobyan <taguhi.hakobyan@sololearn.com>
2022-01-17 23:19:00 +0400

    "Solved Common Character Count"

--- exercices/commonCharacterCount.js
+++ exercices/commonCharacterCount.js
@@ -4,6 +4,22 @@
 // solution(s1, s2) = 3.
 
 function getCommonCharactersCount(s1, s2) {
-    console.log(s1)
-    
+    let count = 0;
+    let string1 = s1.split("");
+    let string2 = s2.split("");
+    for(let i = 0; i < string1.length; i++){
+        for(let j = 0; j < string2.length; j++){
+            if(string1[i] == string2[j]){
+                count++
+                string2.splice(j, 1)
+                break;
+            }
+        }
+    }
+    console.log(count)
 }
+
+let s1 = "aabcc";
+let s2 = "adcaa";
+
+getCommonCharactersCount(s1, s2)
